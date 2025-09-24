b981eb94704f6343fc2981b7378444d3bf25282e
Edmont Budakyan <edmont@abv.bg>
2025-09-24 23:47:20 +0300

    Add files via upload

---
+++ Codewars/8 kyu/Simple multiplication.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/583710ccaa6717322c000105/train/python
+
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+def simple_multiplication(number) :
+    return number * 8 if number % 2 == 0 else number * 9
+#
+def simple_multiplication(n) :
+    return n * (8 + n%2)
