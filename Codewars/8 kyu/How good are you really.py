b1c6d3966661a9005df08d3000f2686f396517e5
Edmont Budakyan <edmont@abv.bg>
2025-10-05 20:48:48 +0300

    add snippets

---
+++ Codewars/8 kyu/How good are you really.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/5601409514fc93442500010b/train/python
+
+# There was a test in your class and you passed it. Congratulations!
+
+# But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+# You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+# Return true if you're better, else false!
+
+# Note:
+# Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+    
+def better_than_average(class_points, your_points):
+    return your_points > sum(class_points) / len(class_points)
