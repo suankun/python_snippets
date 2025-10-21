d077eafabd20644a1ab3d0e232c1cf7e1cec0ffd
Edmont Budakyan <edmont@abv.bg>
2025-10-21 07:10:16 +0300

    Add files via upload

---
+++ Codewars/8 kyu/Grasshopper - Basic Function Fixer.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/56200d610758762fb0000002/train/python
+
+# Fix the function
+# I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+# Can you help me fix the function?
+
+def add_five(num):
+    num += 5
+    return num
+#
+# def add_five(num):
+#     return num + 5
