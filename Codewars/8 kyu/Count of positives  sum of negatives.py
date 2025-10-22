c8d0aef439c30400bc6c3c073a9062ceb76a2057
Edmont Budakyan <edmont@abv.bg>
2025-10-22 06:47:10 +0300

    Add files via upload

---
+++ Codewars/8 kyu/Count of positives  sum of negatives.py
@@ -0,0 +1,31 @@
+# https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/python
+
+# Given an array of integers.
+
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+# If the input is an empty array or is null, return an empty array.
+
+# Example
+# For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+def count_positives_sum_negatives(arr):
+    if len(arr) == 0:
+        return []    
+    
+    pos = 0;
+    neg = 0;
+    
+    for i in arr:
+        if i > 0:
+            pos += 1
+        else:
+            neg += i
+    
+    return [pos, neg]
+#
+
+# def count_positives_sum_negatives(arr):
+#     pos = sum(1 for x in arr if x > 0)
+#     neg = sum(x for x in arr if x < 0)
+#     return [pos, neg] if len(arr) else []
