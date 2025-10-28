6126a1c031a08d484bfd95702d0815c937a20d92
Edmont Budakyan <edmont@abv.bg>
2025-10-28 07:28:00 +0200

    Add files via upload

---
+++ Codewars/8 kyu/Sum of positive.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+#
+# Task
+# You get an array of numbers, return the sum of all of the positives ones.
+#
+# Example
+# [1, -4, 7, 12] => 1 + 7 + 12 = 20
+# Note
+# If there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    sum = 0
+    for num in arr:
+        if num > 0:
+            sum += num
+    return sum
+#
+# def positive_sum(arr):
+#     return sum(x for x in arr if x > 0)
