078c317b3b210f3cc544fcf9750baff949de148e
Edmont Budakyan <edmont@abv.bg>
2025-10-31 07:18:13 +0200

    Add files via upload

---
+++ Codewars/8 kyu/MakeUpperCase.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/python
+# 
+# Write a function which converts the input string to uppercase.
+
+def make_upper_case(s):
+    return s.upper()
